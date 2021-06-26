32d4a1a3b6da6981140380424d72a41c24635923
김수환 <reiy0902@naver.com>
2021-06-26 20:26:37 +0900

    Step 1. Making Console and Chat

---
+++ Assets/Scripts/UI/UI_ConsoleLine.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_ConsoleLine : MonoBehaviour
+{
+    [SerializeField] private RectTransform rectTransform;
+    [SerializeField] private TextMeshProUGUI text_Content;
+    public float Height
+    {
+        get
+        {
+            return rectTransform.sizeDelta.y;
+        }
+    }
+
+    public UI_ConsoleLine SetContent(string _content)
+    {
+        text_Content.SetText(_content);
+        rectTransform.offsetMax = new Vector2(0, rectTransform.offsetMax.y);
+        rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 0, text_Content.preferredHeight);
+        return this;
+    }
+
+    public UI_ConsoleLine SetParent(Transform _transform)
+    {
+        rectTransform.SetParent(_transform);
+        return this;
+    }
+
+    public UI_ConsoleLine SetPosition(float _y)
+    {
+        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, _y);
+        return this;
+    }
+}
